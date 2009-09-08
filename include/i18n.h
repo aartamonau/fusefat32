e8818847de6d38057e5d0b335e6428e777c32f12
Aliaksiej Artamonaŭ <aliaksiej.artamonau@gmail.com>
2009-09-08 23:37:28 +0300

    - 'include/i18n.h' header with localization shortcuts

---
+++ include/i18n.h
@@ -0,0 +1,20 @@
+/**
+ * @file   i18n.h
+ * @author  <aliaksiej.artamonau@gmail.com>
+ * @date   Tue Sep  8 21:44:35 2009
+ * 
+ * @brief  internatinalization related functions
+ *
+ * At the time this file just includes @em libintl.h header and defines
+ * underscore shortcut for @em gettext function.
+ * 
+ */
+#ifndef _I18N_H_
+#define _I18N_H_
+
+#include <libintl.h>
+
+/// @em gettext function shortcut
+#define _(...) gettext(__VA_ARGS__)
+
+#endif 
